7b466ad5b5de27a57c6d88330c77c71514429a5e
James Armes <jarmes@codeforamerica.org>
2024-08-05 12:40:08 -0400

    Fixed linting error.

--- lib/destination/one_drive.rb
+++ lib/destination/one_drive.rb
@@ -8,7 +8,7 @@ module DocumentTransfer
     # Microsoft OneDrive destination.
     class OneDrive < Base
       def transfer(source)
-        result = service.upload(source, path: @config.path, filename: @config.filename)
+        service.upload(source, path: @config.path, filename: @config.filename)
 
         { path: File.join(@config.path, @config.filename) }
       rescue Microsoft::Graph::Error => e
